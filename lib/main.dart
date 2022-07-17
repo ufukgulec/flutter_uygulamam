0abff928221b2fc2dee9c930a0dcff6e442484d9
Ufuk Güleç <oug505@gmail.com>
2022-07-17 22:18:09 +0300

    ListView.builder ile list kullanıldı

--- lib/main.dart
+++ lib/main.dart
@@ -13,31 +13,42 @@ class MyApp extends StatelessWidget {
   int not = 20;
   @override
   Widget build(BuildContext context) {
+    var ogrenciler = ["Ufuk Güleç", "Yunus Başer", "Umut Yıldırım"];
     //Widget'ın çalışan kod bloğu
     return Scaffold(
       // Widget
       appBar: AppBar(
         title: Text(baslik),
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text("Sonucu gör"),
-          onPressed: () {
-            if (not >= 50) {
-              mesaj = "Dersten geçti";
-            } else if (not >= 40) {
-              mesaj = "Büte kaldı";
-            } else {
-              mesaj = "Dersten kaldı";
-            }
-            var alert = AlertDialog(
-              title: Text("Sınav sonucu"),
-              content: Text(mesaj),
-            );
-            showDialog(
-                context: context, builder: (BuildContext context) => alert);
-          },
-        ),
+      body: Column(
+        children: [
+          Expanded(
+              child: ListView.builder(
+                  itemCount: ogrenciler.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Text(ogrenciler[index]);
+                  })),
+          Center(
+            child: ElevatedButton(
+              child: const Text("Sonucu gör"),
+              onPressed: () {
+                if (not >= 50) {
+                  mesaj = "Dersten geçti";
+                } else if (not >= 40) {
+                  mesaj = "Büte kaldı";
+                } else {
+                  mesaj = "Dersten kaldı";
+                }
+                var alert = AlertDialog(
+                  title: const Text("Sınav sonucu"),
+                  content: Text(mesaj),
+                );
+                showDialog(
+                    context: context, builder: (BuildContext context) => alert);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
